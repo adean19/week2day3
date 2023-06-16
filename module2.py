3dee41986c640eaff63001abcc53d60ba480b071
Nickero Dean <nickerodean@Nickeros-MacBook-Air.local>
2023-06-15 18:19:29 -0700

    Made the inputs integers so that they can be multipled together

--- module2.py
+++ module2.py
@@ -3,9 +3,13 @@ from math import pi as p
 def sqft_house():
     length = input("What's the length?")
     width = input("What's the width?")
-    squareft = length * width
+    squareft = int(length) * int(width)
     print (squareft)
 
 def circle_circum():
     r = input("From the middle of the circle to any point on the boundary, please provide the radius.")
-    C = 2*p*r
+    C = 2*p*int(r)
+    print (C)
+
+sqft_house()
+circle_circum()
